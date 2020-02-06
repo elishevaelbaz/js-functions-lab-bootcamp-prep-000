21d2addc80bf624c2e84b6442f4fac89bc000546
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-02-06 00:05:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,25 +6,14 @@
     return "Happy holidays!";
   }
   
+  function happyHolidaysTo(name){
+    return `Happy holidays, ${name}!`
+  }
+  
+  function happyCustomHolidayTo(holiday, name){
+    return `Happy ${holiday}, ${name}!`
+  }
 
-//   describe('happyHolidaysTo(name)', () => {
-//     it('returns "Happy holidays, ${name}!"', () => {
-//       expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
-//       expect(happyHolidaysTo('everyone')).toEqual('Happy holidays, everyone!')
-//     })
-//   })
-
-//   describe('happyCustomHolidayTo(holiday, name)', () => {
-//     it('returns "Happy ${holiday}, ${name}!"', () => {
-//       expect(happyCustomHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
-//       expect(happyCustomHolidayTo('New Year', 'everyone')).toEqual('Happy New Year, everyone!')
-//     })
-//   })
-
-//   describe('holidayCountdown(holiday, days)', () => {
-//     it('returns "It\'s ${days} days until ${holiday}!"', () => {
-//       expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
-//       expect(holidayCountdown("Father's Day", 120)).toEqual("It's 120 days until Father's Day!")
-//     })
-//   })
-// })
+ function holidayCountdown(holiday, days){
+   return `Its ${days} days until ${holiday}!`
+ }
